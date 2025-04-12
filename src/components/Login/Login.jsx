64c817571cf54aa4a070a0a589689e0ad7dc2f18
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-04-12 21:16:37 +0600

    email registration,login etc explored

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,117 @@
+/* import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase.init";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa"; */
+
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../firebase.init";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return (
-        <div>
-    <h2 className="text-4xl">Log in</h2>            
+  const [success, setSuccess] = useState(false);
+  const [logInError, setLogInError] = useState("");
+  const emailRef = useRef();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    // Reset status
+    setSuccess(false);
+    setLogInError("");
+    // login user
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+
+        if (!result.user.emailVerified) {
+          setLogInError("Please verify your email address");
+        } else {
+          setSuccess(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setLogInError(error.message);
+      });
+  };
+
+  const handleForgetPassWord = () => {
+    const email = emailRef.current.value;
+    console.log(email);
+    if (!email) {
+      alert(`Please provide a valid email address`);
+    }
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        // Password reset email sent!
+        // ..
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
+            <fieldset className="fieldset">
+              <label className="fieldset-label">Email</label>
+              <input
+                ref={emailRef}
+                type="email"
+                className="input"
+                name="email"
+                placeholder="Email"
+              />
+              <label className="fieldset-label">Password</label>
+              <input
+                type="password"
+                className="input"
+                name="password"
+                placeholder="Password"
+              />
+              <div onClick={handleForgetPassWord}>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </form>
+          {success && (
+            <p className="text-green-600 text-center py-5">Login Successful</p>
+          )}
+
+          {logInError && (
+            <p className="text-red-500">
+              {logInError.replace("Firebase:", "")}
+            </p>
+          )}
+
+          <p>
+            New to this website please{" "}
+            <Link className="link" to="/signUp">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,21 +1,45 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
 import { auth } from "../../firebase.init";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [success, setSuccess] = useState(false);
+  const [showPass, setShowPass] = useState(false);
   const handleSignUp = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
+    const terms = e.target.terms.checked;
+    const name = e.target.name.value;
+    const photo = e.target.photo.value;
 
-    if (password.length < 6) {
+    console.log(name, photo);
+
+    /*     const passwordPattern =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+    if (!passwordPattern.test(password)) {
+      return alert(
+        `Password must be 6 character, and have one uppercase,one lowercase one number, one special character`
+      );
+    } */
+    /* if (password.length < 6) {
       setErrorMessage(`Password Should Be 6 characters or longer`);
       return;
-    }
-
+    } */
     setErrorMessage("");
+
+    if (!terms) {
+      setErrorMessage("Please accept our terms and conditions");
+      return;
+    }
 
     setSuccess(false);
 
@@ -23,17 +47,31 @@ const SignUp = () => {
       .then((result) => {
         console.log(result.user);
         setSuccess(true);
+
+        // send verification email address
+
+        sendEmailVerification(auth.currentUser).then(() => {
+          console.log("Verification Email Sent");
+        });
+
+        // Update Profile name and photoURL
+        const profile = {
+          displayName: name,
+          photoURL: photo,
+        };
+        updateProfile(auth.currentUser, profile)
+          .then(() => {
+            console.log("User Profile Updated");
+          })
+          .catch((error) => {
+            console.log("Profile Update Error", error);
+          });
       })
       .catch((error) => {
         console.log("Error", error.message);
         setErrorMessage(error.message);
         setSuccess(false);
       });
-    /* .catch((error) => {
-        console.log("Error", error.message))
-    setErrorMessage(error.message)
-      })
-     */
   };
 
   return (
@@ -41,6 +79,22 @@ const SignUp = () => {
       <form onSubmit={handleSignUp} className="card-body">
         <h3 className="text-3xl text-center font-bold">Sign Up now!</h3>
         <fieldset className="fieldset">
+          <label className="fieldset-label">Name</label>
+          <input
+            name="name"
+            type="text"
+            className="input"
+            placeholder="name"
+            required
+          />
+          <label className="fieldset-label">photo URL</label>
+          <input
+            name="photo"
+            type="text"
+            className="input"
+            placeholder="photo"
+            required
+          />
           <label className="fieldset-label">Email</label>
           <input
             name="email"
@@ -50,23 +104,42 @@ const SignUp = () => {
             required
           />
           <label className="fieldset-label">Password</label>
-          <input
-            name="password"
-            type="password"
-            className="input"
-            required
-            placeholder="Password"
-          />
-          <div>
-            <a className="link link-hover">Forgot password?</a>
+          <div className="relative">
+            <input
+              name="password"
+              // type="password"
+              type={showPass ? "text" : "password"}
+              className="input"
+              required
+              placeholder="Password"
+            />
+            <button
+              onClick={() => setShowPass(!showPass)}
+              className="btn  btn-xs right-6 top-3  absolute"
+            >
+              {showPass ? <FaEyeSlash /> : <FaEye />}
+            </button>
           </div>
+
+          <fieldset className="fieldset p-4 bg-base-100 border border-base-300 rounded-box ">
+            <label className="fieldset-label">
+              <input type="checkbox" name="terms" className="checkbox" />
+              Accept Our Terms & Conditions
+            </label>
+          </fieldset>
           <button className="btn btn-neutral mt-4">Sign Up</button>
         </fieldset>
       </form>
+
       {errorMessage && <p className="text-red-600">{errorMessage}</p>}
-      {
-        success && <p className="text-green-600">Successfully created User </p>
-      }
+      {success && <p className="text-green-600">Successfully created User </p>}
+
+      <p className="m-2">
+        Already have an account ? Please{" "}
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </p>
     </div>
   );
 };
